cc52518efc0c5838ae450da2deda7291fdd3e3d0
Daniil Zaburunov <zaburunov.2000@mail.ru>
2024-10-03 13:16:30 +0400

    [frontend]: сделал рефакторинг ShopItem

---
+++ vite-project/src/components/ShopItem.jsx
@@ -0,0 +1,29 @@
+import { useSelector } from "react-redux";
+
+function ShopItem() {
+  const product = useSelector((state) => state.product);
+  return (
+    <div>
+      <h2>List of Shop</h2>
+      {product.loading && <div>Loading...</div>}
+      {!product.loading && product.error ? (
+        <div>Error: {product.error}</div>
+      ) : null}
+      {!product.loading && product.shop.length ? (
+        <ul>
+          {product.shop.map((item) => (
+            <>
+              <div key={item.id}>
+                <li>{item.title}</li>
+                <img style={{ width: "10%" }} src={item.image} alt="dw" />
+                <li>{item.price}</li>
+              </div>
+            </>
+          ))}
+        </ul>
+      ) : null}
+    </div>
+  );
+}
+
+export { ShopItem };
--- vite-project/src/shop/ShopView.jsx
+++ vite-project/src/shop/ShopView.jsx
@@ -1,32 +1,16 @@
 import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { fetchGoods } from "./shopSlice";
+import { ShopItem } from "../components/ShopItem";
 
 export const ShopView = () => {
-  const product = useSelector((state) => state.product);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchGoods());
   }, []);
   return (
-    <div>
-      <h2>List of Shop</h2>
-      {product.loading && <div>Loading...</div>}
-      {!product.loading && product.error ? (
-        <div>Error: {product.error}</div>
-      ) : null}
-      {!product.loading && product.shop.length ? (
-        <ul>
-          {product.shop.map((item) => (
-            <>
-              <div key={item.id}>
-                <li>{item.title}</li>
-                <li>{item.price}</li>
-              </div>
-            </>
-          ))}
-        </ul>
-      ) : null}
-    </div>
+    <>
+      <ShopItem />
+    </>
   );
 };
